62234edc554cc5cf08e730e33fa386b6c09129c8
Katargina <katrin-kate@ukr.net>
2026-08-10 01:25:42 +0200

    hero mobile added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { DM_Sans, Manrope, Noto_Sans } from "next/font/google";
+import { DM_Sans, Manrope, Noto_Sans, Merienda} from "next/font/google";
 import { QueryProvider } from "@/providers/QueryProvider";
 import "./globals.css";
 
 const manrope = Manrope({
   variable: "--font-manrope",
-  subsets: ["latin", "cyrillic"],
+  subsets: ["latin", "latin-ext"],
 });
 
 const dmSans = DM_Sans({
@@ -15,7 +15,12 @@ const dmSans = DM_Sans({
 
 const notoSans = Noto_Sans({
   variable: "--font-noto-sans",
-  subsets: ["latin", "cyrillic"],
+  subsets: ["latin", "latin-ext"],
+});
+
+const merienda = Merienda({
+  variable: "--font-merienda",
+  subsets: ["latin", "latin-ext"],
 });
 
 export const metadata: Metadata = {
@@ -27,7 +32,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="uk"
-      className={`${manrope.variable} ${dmSans.variable} ${notoSans.variable}`}
+      className={`${manrope.variable} ${dmSans.variable} ${notoSans.variable} ${merienda.variable}`}
     >
       <body>
         <QueryProvider>{children}</QueryProvider>
