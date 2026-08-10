d578524f1ced62a486a2524a3fc59a5537b8a7fb
Roman Pasichniy <roman.pasichniy@gmail.com>
2026-08-10 22:40:26 +0300

    resolve conflicts

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 import LoginForm from "@/components/auth/LoginForm/LoginForm";
 import Container from "@/components/common/Container/Container";
-import Logo from "@/components/common/Logo/Logo";
 import styles from "./page.module.css";
 
 export default function LoginPage() {
